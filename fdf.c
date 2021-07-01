375422908ab9f11f24ff37be4618b54c165ce6e6
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-07-01 23:49:11 +0200

    more math

--- fdf.c
+++ fdf.c
@@ -6,6 +6,7 @@ int	main()
 	void	*mlx_win;
 	t_data	img;
 	t_points	points;
+	int	scale_factor;
 	int	a;
 	int	b;
 	int	l;
@@ -18,9 +19,23 @@ int	main()
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, 
 			&img.line_length, &img.endian);
 	points.color = 0xff;
-	c = 3000;
-	l = 800;
+	c = 100;
+	l = 100;
 
+	if (c >= 45)
+		scale_factor = ((800 - (800/c) - ((800 * 800)/(c * c)) + 2)/c);
+	else if (c >= 20)
+		scale_factor = (15 - (c*c)/500);
+	else if (c >= 10)
+		scale_factor = (25 - (c*c)/150);
+	else if (c >= 7)
+		scale_factor = (50 - (c*c)/100);
+	else if (c >= 5)
+		scale_factor = (60 - (c * c)/100);
+	else
+		scale_factor = (100 - (c * c) /10);
+	c = scale_factor * c;
+	l = scale_factor * l;
 	a = l/2;
 	b = c/2;
 	points.xi = (a + b) * cos (M_PI/6) + 1080;
