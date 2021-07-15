d3096a63e1b708d260fe3121128817e953d00b46
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-07-15 21:58:16 +0200

    a

--- srcs_bonus/drawer_bonus.c
+++ srcs_bonus/drawer_bonus.c
@@ -57,17 +57,20 @@ void	draw_iso_right(t_points *pnt, t_data *data, int *row, int *col)
 	double	row_cent;
 	double	col_cent;
 
-	row_cent = (double)*row - ((double)(data->row) / 2.0);
-	col_cent = (double)*col - ((double)(data->col) / 2.0);
+	row_cent = (double)*row - ((double)(data->row - 1) / 2.0);
+	col_cent = (double)*col - ((double)(data->col - 1) / 2.0);
 	rotation(&row_cent, &col_cent, data->rot_angle); 
 	pnt->xi = (int)(data->s_fct
 			* ((row_cent + col_cent) * cos (M_PI / 6)) + 1080);
 	pnt->yi = (int)(data->s_fct
 			* (-pnt->zi + (col_cent - row_cent) * sin (M_PI / 6)) + 680);
+	row_cent = (double)*row - ((double)(data->row - 1) / 2.0);
+	col_cent = (double)(*col + 1) - ((double)(data->col - 1) / 2.0);
+	rotation(&row_cent, &col_cent, data->rot_angle); 
 	pnt->xf = (int)(data->s_fct
-			* ((row_cent + col_cent + 1) * cos (M_PI / 6)) + 1080);
+			* ((row_cent + col_cent) * cos (M_PI / 6)) + 1080);
 	pnt->yf = (int)(data->s_fct
-			* (-pnt->zf + (col_cent + 1 - row_cent) * sin (M_PI / 6)) + 680);
+			* (-pnt->zf + (col_cent - row_cent) * sin (M_PI / 6)) + 680);
 	draw_line(pnt, data);
 }
 
@@ -78,17 +81,20 @@ void	draw_iso_top(t_points *pnt, t_data *data, int *row, int *col)
 	double	row_cent;
 	double	col_cent;
 
-	row_cent = (double)*row - ((double)(data->row) / 2.0);
-	col_cent = (double)*col - ((double)(data->col) / 2.0);
+	row_cent = (double)*row - ((double)(data->row - 1) / 2.0);
+	col_cent = (double)*col - ((double)(data->col - 1) / 2.0);
 	rotation(&row_cent, &col_cent, data->rot_angle); 
 	pnt->xi = (int)(data->s_fct
 			* ((row_cent + col_cent) * cos (M_PI / 6)) + 1080);
 	pnt->yi = (int)(data->s_fct
 			* (-pnt->zi + (col_cent - row_cent) * sin(M_PI / 6)) + 680);
+	row_cent = (double)(*row + 1) - ((double)(data->row - 1) / 2.0);
+	col_cent = (double)*col - ((double)(data->col - 1) / 2.0);
+	rotation(&row_cent, &col_cent, data->rot_angle); 
 	pnt->xf = (int)(data->s_fct
-			* ((row_cent + col_cent + 1) * cos (M_PI / 6)) + 1080);
+			* ((row_cent + col_cent) * cos (M_PI / 6)) + 1080);
 	pnt->yf = (int)(data->s_fct
-			* (-pnt->zf + (col_cent - row_cent - 1) * sin(M_PI / 6)) + 680);
+			* (-pnt->zf + (col_cent - row_cent) * sin(M_PI / 6)) + 680);
 	draw_line(pnt, data);
 }
 
